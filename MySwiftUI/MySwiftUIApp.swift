d2de1cb35fb89823b21912fc7482b2aa190ccb18
David Yoon <qqpo12@naver.com>
2023-03-03 23:31:53 +0900

    Initial Commit

---
+++ MySwiftUI/MySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySwiftUIApp.swift
+//  MySwiftUI
+//
+//  Created by Jiwon Yoon on 2023/03/03.
+//
+
+import SwiftUI
+
+@main
+struct MySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
